62c0048b542e6265cf787a36bba60f55e0098bd1
Rajat Bhatnagar <Rajat_Bhatnagar@outlook.com>
2021-12-05 14:14:19 +0530

    Squares of sorted array solution using O(n) instead of O(n log(n)) complexity

--- Dronacharya.java
+++ Dronacharya.java
@@ -2,8 +2,9 @@ import java.util.Arrays;
 
 public class Dronacharya {
     public static void main(String[] args) {
-        int[] numbers = {100, -1, -2, 0, 3, 4, 9, 6};
-        System.out.println("Sorted Array - " + Arrays.toString(numbers) + " after method call- " + Arrays.toString(sortIntArr(numbers)));
+        int[] numbers = {-2, -1, 0, 3, 4, 6, 9};
+        System.out.println("Sorted Array O(n log (n)) - " + Arrays.toString(numbers) + " after method call- " + Arrays.toString(sortIntArr(numbers)));
+        System.out.println("Sorted Array O(n) - " + Arrays.toString(numbers) + " after method call- " + Arrays.toString(sortIntArrayUsingRightAndLeftPointers(numbers)));
     }
 
     private static int[] sortIntArr(int[] numbers) {
@@ -11,5 +12,26 @@ public class Dronacharya {
         return Arrays.stream(numbers)
                 .sorted()
                 .toArray();
+    }
+
+    // This is O(n) performance on data sets
+    private static int[] sortIntArrayUsingRightAndLeftPointers(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int square;
+            if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+                square = nums[right];
+                right--;
+            } else {
+                square = nums[left];
+                left++;
+            }
+            result[i] = square * square;
+        }
+        return result;
     }
 }
